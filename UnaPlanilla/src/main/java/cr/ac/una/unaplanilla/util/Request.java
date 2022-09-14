00dc8b6170fc0766ec5635281da5336a1fd85080
Arauz <86898345+kjarj54@users.noreply.github.com>
2022-09-13 18:30:30 -0600

    cambios

--- UnaPlanilla/src/main/java/cr/ac/una/unaplanilla/util/Request.java
+++ UnaPlanilla/src/main/java/cr/ac/una/unaplanilla/util/Request.java
@@ -5,20 +5,20 @@
  */
 package cr.ac.una.unaplanilla.util;
 
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.Invocation;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.GenericType;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.MultivaluedHashMap;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.Response;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Platform;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedHashMap;
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.core.Response;
 
 /**
  *
--- UnaPlanilla/src/main/java/module-info.java
+++ UnaPlanilla/src/main/java/module-info.java
@@ -3,11 +3,18 @@ module cr.ac.una.unaplanilla {
     requires javafx.fxml;
     requires com.jfoenix;
     requires java.logging;
+    requires jakarta.ws.rs;
+    requires jakarta.xml.bind;
+    requires java.sql;
+    requires java.base;
+    requires jakarta.json;
+
     
     //TODO
     
-    opens cr.ac.una.unaplanilla to javafx.fxml;
+    opens cr.ac.una.unaplanilla to javafx.fxml,javafx.graphics;
     opens cr.ac.una.unaplanilla.controller to java.base,javafx.fxml,javafx.base,com.jfoenix;
     opens cr.ac.una.unaplanilla.util to java.base,javafx.fxml,javafx.base,com.jfoenix;
     
+    exports cr.ac.una.unaplanilla.model;
 }
