e1fdd198f6b54915acc8a790c96570951ae04987
Arauz <86898345+kjarj54@users.noreply.github.com>
2022-09-20 17:14:46 -0600

    1.0

--- UnaPlanillaWs/src/main/java/cr/ac/una/unaplanillaws/controller/EmpleadoController.java
+++ UnaPlanillaWs/src/main/java/cr/ac/una/unaplanillaws/controller/EmpleadoController.java
@@ -9,24 +9,16 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.GenericEntity;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import cr.ac.una.unaplanillaws.model.EmpleadoDto;
 import cr.ac.una.unaplanillaws.service.EmpleadoService;
 import cr.ac.una.unaplanillaws.util.CodigoRespuesta;
-import cr.ac.una.unaplanillaws.util.JwTokenHelper;
 import cr.ac.una.unaplanillaws.util.Respuesta;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.SecurityContext;
-import cr.ac.una.unaplanillaws.util.Secure;
 
 /**
  *
